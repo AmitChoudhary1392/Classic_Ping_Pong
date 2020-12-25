1728d50019a038a230a8dd3b41ba60b2269534e3
Amit Choudhary <53244875+AmitChoudhary1392@users.noreply.github.com>
2020-12-25 00:46:56 -0500

    Creating Task-2
    create 2 paddles and a ball. Set starting coordinates.

--- PingPong.py
+++ PingPong.py
@@ -22,6 +22,44 @@ window.bgcolor("black")                 #window background color
 window.setup(width=800, height=600)
 window.tracer(0)                        #accelerate the graphics without delays
 
+# Build Paddles and Ball
+
+## Left Paddle
+Paddle_L = turtle.Turtle()        #initialise turtle object by accessing Turtle class
+Paddle_L.speed(0)               #sets speed of animation based on argument, 0-max speed/zero delay
+
+### Set paddle features
+Paddle_L.shape("square")
+Paddle_L.color("blue")
+Paddle_L.shapesize(stretch_wid=5, stretch_len=1)        #default size of square shape is 20x20 px
+Paddle_L.penup()                #move the turtle without a trace
+
+### Set starting coordinates for the left paddle. Default (0,0) for turtle object
+Paddle_L.goto(-350,0)
+
+## Right Paddle
+Paddle_R = turtle.Turtle()       #initialise turtle object by accessing Turtle class
+Paddle_R.speed(0)               #sets speed of animation based on argument, 0-max speed/zero delay
+
+### Set paddle features
+Paddle_R.shape("square")
+Paddle_R.color("red")
+Paddle_R.shapesize(stretch_wid=5, stretch_len=1)        #default size of square shape is 20x20 px
+Paddle_R.penup()                #move the turtle without a trace
+
+### Set starting coordinates for the left paddle. Default (0,0) for turtle object
+Paddle_R.goto(350,0)
+
+## Ball
+ball= turtle.Turtle()
+ball.speed(0)
+
+### Set ball features
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+### no need to define starting point. Default is (0,0)
+
 
 #initialize the main game loop
 while True:
